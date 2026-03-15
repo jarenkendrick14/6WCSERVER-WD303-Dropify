8152aa849932fd51d0ccfdf5d290592ed4774338
Jaren Kendrick <jarenkendricky@gmail.com>
2026-03-16 07:14:25 +0800

    added missing https:// on api.js

--- vue-project/src/config/api.js
+++ vue-project/src/config/api.js
@@ -1,5 +1,5 @@
 // Centralized API configuration
 // Change this ONE value when switching between local dev and production
-const API_BASE_URL = '6wcserver-wd303-dropify-production.up.railway.app';
+const API_BASE_URL = 'https://6wcserver-wd303-dropify-production.up.railway.app';
 
 export default API_BASE_URL;
